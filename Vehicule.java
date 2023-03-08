76685a5459c732dca7b1eef4c23a4198b67616aa
Khrafa Ayke <strummer.notesrain@gmail.com>
2023-03-08 06:38:03 +0000

    created a petrol engine variable

--- Vehicule.java
+++ Vehicule.java
@@ -1,8 +1,11 @@
 public class Vehicule {
 
+  
+  private PetrolEngine pE;
     public static void main (String[] args){
   
   //PetrolEngine pE = new PetrolEngine();
    System.out.println("Hello");
+   pE.start();
     }
 }
