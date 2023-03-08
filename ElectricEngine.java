688029e4bab1a9539587c6e728e7444a569751de
Khrafa Ayke <strummer.notesrain@gmail.com>
2023-03-08 09:47:25 +0000

    new method incorporated in ElectricEngine class

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -2,4 +2,7 @@ public class ElectricEngine {
     public void start(){
         System.out.println("Electric Vehicule Started");
     }
+    public void stop(){
+        System.out.println("Electric Vehicule Stopped");
+    }
 }
