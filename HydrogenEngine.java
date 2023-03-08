014cad938c735a859934a475654fc995d60184fb
Khrafa Ayke <strummer.notesrain@gmail.com>
2023-03-08 10:30:03 +0000

    Second method added

--- HydrogenEngine.java
+++ HydrogenEngine.java
@@ -2,5 +2,8 @@ public class HydrogenEngine {
     public void start(){
         System.out.println("Vroooom!");
     }
+    public void stop(){
+        System.out.println("Hydroengine stopped");
+    }
     
 }
